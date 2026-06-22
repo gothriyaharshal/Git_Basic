1ea003c41ca97f8e63b8c74b4e4ef0717afb5760
Harshal Gothriya <gothriyaharshal14@gmail.com>
2026-06-22 18:17:03 +0530

    Hurry up

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Welcome
+{
+public static void main(String[] args)
+{
+Scanner sc= new Scanner(System.in);
+
+int a = 10;
+int b = 10;
+
+int sum = a+b;
+
+System.out.println(a+b);
+
+}
+}
