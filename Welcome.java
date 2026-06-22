617201a0821274643afedc0d7195dc2934831dc4
Harshal Gothriya <gothriyaharshal14@gmail.com>
2026-06-22 18:34:50 +0530

    feature 1 commit and merging in main branch

--- Welcome.java
+++ Welcome.java
@@ -6,12 +6,12 @@ public static void main(String[] args)
 {
 Scanner sc= new Scanner(System.in);
 
-int a = 10;
+int a = 1990;   //branch feature1
 int b = 10;
 
 int sum = a+b;
 
-System.out.println(a+b);
+System.out.println(sum);
 
 }
 }
